ea55ed63436b585da7346b318cdb1bf0d682bf3f
Aaron Jones <ronj2005@gmail.com>
2021-10-28 00:12:09 -0400

    corrected readme files second 10/27

--- index.js
+++ index.js
@@ -1,13 +1,81 @@
-// TODO: Include packages needed for this applications
+// Included packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js")
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Created an array of questions for user input
+inquirer
+    .prompt([
+     {   
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    {
+        type: 'input',
+        message: 'Please give a brief decription of your project.',
+        name: 'description',
+    },
 
-// TODO: Create a function to initialize app
-function init() {}
+    {
+        type: 'checkbox',
+        message: 'Please select which language(s) you use.',
+        choices: ["HTML", " CSS", " Javascript", " Python", " Java", "Node", "React"],
+        name: 'languages',
+    },
 
-// Function call to initialize app
-init();
+    {
+        type: 'input',
+        message: 'Please provide installation instructions.',
+        name: 'installInstructions',
+    },
+
+    {
+        type: 'input',
+        message: 'Please provide usage information.',
+        name: 'usageInformation',
+    },
+
+    {
+        type: 'input',
+        message: 'Please provide contribution guidelines.',
+        name: 'contributionGuidelines',
+    },
+
+    {
+        type: 'input',
+        message: 'Please provide test instructions:',
+        name: 'testInstructions',
+    },
+
+    {
+        type: 'list',
+        message: 'Please select provided license(s)',
+        choices: ["Apache License 2.0", "MIT License","Boost Software License 1.0"],
+        name: 'license',
+    },
+
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'githubUsername',
+
+    },
+
+    {
+        type: 'input',
+        message: 'What is your e-mail address?',
+        name: 'email',
+
+    },
+
+    ])
+
+   generateMarkdown
+// Created a function to write README file
+   .then((data) => {
+    fs.writeFile("README2.md", generateMarkdown(data), (err) => 
+      err ? console.log(err) : console.log(data)
+    );
+  });
